c004fc5158a6bd29dc192eada1924da86bb591bf
hellojoyworldz <hellojoyworldz@gmail.com>
2024-10-22 23:43:33 +0900

    지연 로딩

--- src/App.jsx
+++ src/App.jsx
@@ -6,33 +6,30 @@ import MainScreen from "./components/MainScreen";
 const App = () => {
   const [webApp, setWebApp] = useState(null);
   const [showSplash, setShowSplash] = useState(true);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   const handleSplashComplete = () => {
     setShowSplash(false);
   };
 
   useEffect(() => {
-    const initTelegramWebApp = () => {
-      if (window.Telegram?.WebApp) {
-        const tg = window.Telegram.WebApp;
-        setWebApp(tg);
-        tg.ready();
-        tg.expand();
-      } else {
-        console.log("Telegram WebApp not found");
-        alert("Telegram WebApp not found");
-      }
-    };
+    if (window.Telegram?.WebApp) {
+      const tg = window.Telegram.WebApp;
+      setWebApp(tg);
+      tg.ready();
+      tg.expand();
+    }
 
-    // 웹앱이 로드되는 데 시간이 걸릴 수 있으므로 약간의 지연을 추가합니다.
-    setTimeout(initTelegramWebApp, 1000);
+    const timer = setTimeout(() => {
+      setIsLoaded(true);
+    }, 2000);
 
-    // 디버깅을 위한 이벤트 리스너 추가
-    window.addEventListener("error", (e) =>
-      console.error("Telegram WebApp error:", e)
-    );
+    return () => clearTimeout(timer);
   }, []);
 
+  if (!isLoaded) {
+    return null; // 또는 로딩 인디케이터를 표시할 수 있습니다.
+  }
   return (
     <>
       {showSplash ? (
