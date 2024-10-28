7a166347a1f212f02ca363d6ae40857d8df2ee69
hellojoyworldz <hellojoyworldz@gmail.com>
2024-10-28 23:24:21 +0900

    간단하게 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,14 @@
 import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import {
-  useHapticFeedback,
-  useShowPopup,
-} from "@vkruglikov/react-telegram-web-app";
 import "./App.css";
 import SplashScreen from "./components/SplashScreen";
 import MainScreen from "./components/MainScreen";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
-  const showPopup = useShowPopup();
-  const [, notificationOccurred] = useHapticFeedback();
-  const [isInvalidVersion, setIsInvalidVersion] = useState(false);
 
   useEffect(() => {
     if (window.Telegram && window.Telegram.WebApp) {
-      if (!window.Telegram.WebApp.isVersionAtLeast("6.9")) {
-        notificationOccurred("error");
-        if (window.Telegram.WebApp.isVersionAtLeast("6.2")) {
-          showPopup({
-            message:
-              "Please update your Telegram app to the latest version to use this app.",
-          });
-        } else {
-          console.log("the version is not supported");
-          setIsInvalidVersion(true);
-        }
-      }
-      // Alternatively to what can be set with react-telegram-web-app, you can directly set the following properties:
-      try {
-        window.Telegram.WebApp.requestWriteAccess();
-      } catch (e) {
-        console.log(e);
-      }
       window.Telegram.WebApp.expand();
     }
 
@@ -42,20 +17,7 @@ const App = () => {
 
   return (
     <Routes>
-      {isInvalidVersion && (
-        <div className="invalid-version">
-          <div className="invalid-version__content">
-            <h1>Sorry but this version is outdated!</h1>
-            <h1>
-              Please update your Telegram app to the latest version to use this
-              app.
-            </h1>
-          </div>
-        </div>
-      )}
-      {!isInvalidVersion && (
-        <Route path="/" element={loading ? <SplashScreen /> : <MainScreen />} />
-      )}
+      <Route path="/" element={loading ? <SplashScreen /> : <MainScreen />} />
     </Routes>
   );
 };
