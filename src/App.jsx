e1f18aa914a2dadecf994c2728fef3f8a4f5c34b
hellojoyworldz <hellojoyworldz@gmail.com>
2024-10-29 00:44:33 +0900

    페이지 로드 후 텔레그램 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,32 @@
-import { useEffect, useState, lazy, Suspense } from "react";
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import WebApp from "@twa-dev/sdk";
 import "./App.css";
 import SplashScreen from "./components/SplashScreen";
-//import MainScreen from "./components/MainScreen";
-
-const MainScreen = lazy(() => import("./components/MainScreen"));
+import MainScreen from "./components/MainScreen";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if (window.Telegram && window.Telegram?.WebApp) {
-      WebApp.ready();
-      WebApp.expand();
-    }
+    window.onload = () => {
+      if (window.Telegram && window.Telegram?.WebApp) {
+        WebApp.ready();
+        WebApp.expand();
+      }
 
-    setTimeout(() => setLoading(false), 3000);
+      setTimeout(() => setLoading(false), 3000);
+    };
+
+    return () => {
+      window.onload = null;
+    };
   }, []);
 
   return (
-    <Suspense fallback={<SplashScreen />}>
-      <Routes>
-        <Route path="/" element={loading ? <SplashScreen /> : <MainScreen />} />
-      </Routes>
-    </Suspense>
+    <Routes>
+      <Route path="/" element={loading ? <SplashScreen /> : <MainScreen />} />
+    </Routes>
   );
 };
 
