17b63a96bfb1c3f0d7ea6361b86bcb6c493eccf2
hellojoyworldz <hellojoyworldz@gmail.com>
2024-10-28 23:21:23 +0900

    수정해보기

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
-import { useLayoutEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import WebApp from "@twa-dev/sdk";
+import {
+  useHapticFeedback,
+  useShowPopup,
+} from "@vkruglikov/react-telegram-web-app";
 import "./App.css";
 import SplashScreen from "./components/SplashScreen";
 import MainScreen from "./components/MainScreen";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
-  useLayoutEffect(() => {
+  const showPopup = useShowPopup();
+  const [, notificationOccurred] = useHapticFeedback();
+  const [isInvalidVersion, setIsInvalidVersion] = useState(false);
+
+  useEffect(() => {
     if (window.Telegram && window.Telegram.WebApp) {
-      WebApp.ready();
-      WebApp.expand();
-      WebApp.enableClosingConfirmation();
+      if (!window.Telegram.WebApp.isVersionAtLeast("6.9")) {
+        notificationOccurred("error");
+        if (window.Telegram.WebApp.isVersionAtLeast("6.2")) {
+          showPopup({
+            message:
+              "Please update your Telegram app to the latest version to use this app.",
+          });
+        } else {
+          console.log("the version is not supported");
+          setIsInvalidVersion(true);
+        }
+      }
+      // Alternatively to what can be set with react-telegram-web-app, you can directly set the following properties:
+      try {
+        window.Telegram.WebApp.requestWriteAccess();
+      } catch (e) {
+        console.log(e);
+      }
+      window.Telegram.WebApp.expand();
     }
 
     setTimeout(() => setLoading(false), 3000);
@@ -19,7 +42,20 @@ const App = () => {
 
   return (
     <Routes>
-      <Route path="/" element={loading ? <SplashScreen /> : <MainScreen />} />
+      {isInvalidVersion && (
+        <div className="invalid-version">
+          <div className="invalid-version__content">
+            <h1>Sorry but this version is outdated!</h1>
+            <h1>
+              Please update your Telegram app to the latest version to use this
+              app.
+            </h1>
+          </div>
+        </div>
+      )}
+      {!isInvalidVersion && (
+        <Route path="/" element={loading ? <SplashScreen /> : <MainScreen />} />
+      )}
     </Routes>
   );
 };
