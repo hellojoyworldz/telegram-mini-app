738c7a10695036253f6e83cc56943b1a26bf7c03
hellojoyworldz <hellojoyworldz@gmail.com>
2024-10-29 02:03:35 +0900

    컴포넌트 expand 주석

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
-// import WebApp from "@twa-dev/sdk";
 import "./App.css";
 import SplashScreen from "./components/SplashScreen";
 import MainScreen from "./components/MainScreen";
@@ -9,24 +8,16 @@ const App = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if (window.Telegram && window.Telegram?.WebApp) {
-      const tg = window.Telegram.WebApp;
-      // 배경 색상 설정
-      //window.Telegram.WebApp.setBackgroundColor("#000");
-      tg.expand();
-      tg.ready();
-    }
+    // if (window.Telegram && window.Telegram?.WebApp) {
+    //   const tg = window.Telegram.WebApp;
+    //   // 배경 색상 설정
+    //   //window.Telegram.WebApp.setBackgroundColor("#000");
+    //   tg.expand();
+    //   tg.ready();
+    // }
     setTimeout(() => {
       setLoading(false);
     }, 3000);
-
-    window.onload = () => {
-      if (window.Telegram && window.Telegram?.WebApp) {
-        const tg = window.Telegram.WebApp;
-        tg.expand();
-        tg.ready();
-      }
-    };
   }, []);
 
   return (
--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -8,7 +8,7 @@ const SplashScreen = () => {
           <div className="animate-bounce mb-4">
             <Camera size={48} className="text-white" />
           </div>
-          <h1 className="text-white text-2xl font-bold">Telegram Mini App</h1>
+          <h1 className="text-white text-2xl font-bold">Telegram Mini App2</h1>
           <div className="mt-4">
             <div className="w-16 h-1 bg-white mx-auto animate-pulse rounded-full"></div>
           </div>
