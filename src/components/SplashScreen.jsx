d27ec7edb4be9d38d80851494fba7482a2b75191
hellojoyworldz <hellojoyworldz@gmail.com>
2024-10-29 01:13:38 +0900

    overflow: hidden;

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -11,12 +11,8 @@ const SplashScreen = () => {
         <div className="mt-4">
           <div className="w-16 h-1 bg-white mx-auto animate-pulse rounded-full"></div>
         </div>
-        <img
-          src="https://static.news.zumst.com/images/127/2024/04/23/dcc926cc063f4a058dcbd2595c18f81c.jpg"
-          alt="Telegram"
-          className="w-32 h-32 mx-auto mt-4 rounded-full"
-        />
       </div>
+      <div style={{ marginTop: "100px" }}>제발 되라</div>
     </div>
   );
 };
