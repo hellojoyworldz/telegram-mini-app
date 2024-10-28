6db8eb9d2d3f37ad82feb25754b1d450c9991b86
hellojoyworldz <hellojoyworldz@gmail.com>
2024-10-29 01:06:23 +0900

    사진으로 길게 만들어보기

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -1,7 +1,6 @@
 import { Camera } from "lucide-react";
 
 const SplashScreen = () => {
-
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-blue-500">
       <div className="text-center">
@@ -12,6 +11,11 @@ const SplashScreen = () => {
         <div className="mt-4">
           <div className="w-16 h-1 bg-white mx-auto animate-pulse rounded-full"></div>
         </div>
+        <img
+          src="https://static.news.zumst.com/images/127/2024/04/23/dcc926cc063f4a058dcbd2595c18f81c.jpg"
+          alt="Telegram"
+          className="w-32 h-32 mx-auto mt-4 rounded-full"
+        />
       </div>
     </div>
   );
