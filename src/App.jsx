17a4ce29bcc98f71f543f0398be380038860918d
hellojoyworldz <hellojoyworldz@gmail.com>
2024-10-29 00:19:22 +0900

    ready, expand만 남기기

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,10 @@ import MainScreen from "./components/MainScreen";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
-  const [frameGeometry, setFrameGeometry] = useState({ width: 0, height: 0 });
 
   useEffect(() => {
-    if (window.Telegram && window.Telegram.WebApp) {
+    if (window.Telegram && window.Telegram?.WebApp) {
       WebApp.ready();
-
-      const frame = WebApp.getFrameGeometry();
-      setFrameGeometry(frame); // 상태에 프레임 정보를 저장
-
-      // UI를 프레임에 맞게 조정
-      console.log("Frame Geometry:", frame);
-
       WebApp.expand();
     }
 
@@ -26,19 +18,9 @@ const App = () => {
   }, []);
 
   return (
-    <div
-      style={{
-        textAlign: "center",
-        padding: "20px",
-        width: `${frameGeometry.width}px`,
-        height: `${frameGeometry.height}px`,
-        overflow: "hidden", // 스크롤 방지
-      }}
-    >
-      <Routes>
-        <Route path="/" element={loading ? <SplashScreen /> : <MainScreen />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={loading ? <SplashScreen /> : <MainScreen />} />
+    </Routes>
   );
 };
 
